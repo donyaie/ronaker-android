c54b239db2220644cdda831d2923fba867f92420
Donyaie <alidonyaie@gmail.com>
2019-05-22 01:19:18 +0300

    +explore product add map and checkout button

--- app/src/main/java/com/ronaker/app/ui/exploreProduct/ExploreProductFragment.kt
+++ app/src/main/java/com/ronaker/app/ui/exploreProduct/ExploreProductFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -55,6 +56,15 @@ class ExploreProductFragment : BaseFragment() {
 
             }
         };
+
+
+
+        productViewModel.checkout.observe(this, Observer { suid ->
+
+            Toast.makeText(context,suid,Toast.LENGTH_LONG).show()
+
+        })
+
 
 
         return binding.root
