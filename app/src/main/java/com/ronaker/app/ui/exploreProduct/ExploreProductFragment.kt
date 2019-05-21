aa41a0f7d15270ac74809168e342d30450bbebae
Donyaie <alidonyaie@gmail.com>
2019-05-22 00:44:24 +0300

    #move product explore fragment to activity

--- app/src/main/java/com/ronaker/app/ui/exploreProduct/ExploreProductFragment.kt
+++ app/src/main/java/com/ronaker/app/ui/exploreProduct/ExploreProductFragment.kt
@@ -4,16 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.ronaker.app.R
 import com.ronaker.app.base.BaseFragment
-import android.view.ViewTreeObserver.OnScrollChangedListener
-import com.ronaker.app.ui.dashboard.DashboardActivity
-import kotlinx.android.synthetic.main.component_toolbar.view.*
-
 
 class ExploreProductFragment : BaseFragment() {
 
@@ -23,26 +18,15 @@ class ExploreProductFragment : BaseFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
 
-        binding = DataBindingUtil.inflate(inflater, com.ronaker.app.R.layout.fragment_explore_product, container, false)
-        productViewModel = ViewModelProviders.of(this).get(ExploreProductViewModel::class.java)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_explore_product, container, false)
+
+        activity?.let {
+            productViewModel = ViewModelProviders.of(it).get(ExploreProductViewModel::class.java)
+            binding.viewModel = productViewModel
+        }
 
 
-        productViewModel.loading.observe(this, Observer { loading ->
-            if (loading) binding.loading.showLoading() else binding.loading.hideLoading()
-        })
-
-
-
-        binding.toolbar.cancelClickListener = View.OnClickListener { (activity as DashboardActivity).backFragment() }
-
-        productViewModel.errorMessage.observe(this, Observer { errorMessage ->
-            if (errorMessage != null) {
-                Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show()
-//                binding.loading.showRetry()
-            } else {
-//                binding.loading.hideRetry()
-            }
-        })
+        binding.toolbar.cancelClickListener = View.OnClickListener { activity ?.onBackPressed() }
 
 
         binding.viewModel = productViewModel
@@ -73,28 +57,10 @@ class ExploreProductFragment : BaseFragment() {
         };
 
 
-        productViewModel.loadProduct(this.arguments!!.getString(SUID_KEY))
-
-
-
-
-
         return binding.root
     }
 
 
-    companion object {
-
-        private var SUID_KEY = "suid"
-
-        fun newInstance(suid: String?): ExploreProductFragment {
-            val bundle = Bundle()
-            bundle.putString(SUID_KEY, suid)
-            val fragment = ExploreProductFragment()
-            fragment.arguments = bundle
-            return fragment
-        }
-    }
 
 
 }
