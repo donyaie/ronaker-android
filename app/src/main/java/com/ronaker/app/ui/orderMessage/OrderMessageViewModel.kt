eef6bb6a44f4d4a61b298da8d0b55d067f15f620
Donyaie <alidonyaie@gmail.com>
2019-08-21 20:09:23 +0300

    +show calcute price

--- app/src/main/java/com/ronaker/app/ui/orderMessage/OrderMessageViewModel.kt
+++ app/src/main/java/com/ronaker/app/ui/orderMessage/OrderMessageViewModel.kt
@@ -1,14 +1,16 @@
 package com.ronaker.app.ui.orderMessage
 
 
+import android.content.Context
 import androidx.lifecycle.MutableLiveData
+import com.ronaker.app.R
 import com.ronaker.app.base.BaseViewModel
 import com.ronaker.app.data.OrderRepository
-import com.ronaker.app.data.ProductRepository
 import com.ronaker.app.data.UserRepository
 import com.ronaker.app.model.Product
 import io.reactivex.disposables.Disposable
 import java.util.*
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class OrderMessageViewModel : BaseViewModel() {
@@ -32,7 +34,9 @@ class OrderMessageViewModel : BaseViewModel() {
     val productPrice: MutableLiveData<String> = MutableLiveData()
 
     val productPriceTitle: MutableLiveData<String> = MutableLiveData()
-
+    @Inject
+    lateinit
+    var context: Context
 
     lateinit var mProduct: Product
     lateinit var mStartDate: Date
@@ -46,52 +50,52 @@ class OrderMessageViewModel : BaseViewModel() {
     }
 
 
-    fun loadProduct(product: Product, startDate: Date, endDAte:Date) {
+    fun loadProduct(product: Product, startDate: Date, endDAte: Date) {
 
         this.mProduct = product
 
-        this.mStartDate=startDate
-        this.mEndDate=endDAte
+        this.mStartDate = startDate
+        this.mEndDate = endDAte
 
+        val days = TimeUnit.DAYS.convert(
+            endDAte.time -
+                    startDate.time,
+            TimeUnit.MILLISECONDS
+        )
 
-
-
-
-
+        productPriceTitle.value = "for $days days"
+        productPrice.value = String.format("%s%.02f", context.getString(R.string.title_curency_symbol), product.price_per_day!! * days)
 
 
     }
 
-    fun checkOut(message:String){
+    fun checkOut(message: String) {
 
 
-        subscription=orderRepository.createOrder(userRepository.getUserToken(),mProduct.suid!!,mStartDate,mEndDate,message)
-            .doOnSubscribe { loading.value = true }
-            .doOnTerminate { loading.value = false }
-            .subscribe { result ->
-                if (result.isSuccess()|| result.isAcceptable()) {
 
-                    next.value=true
+        subscription =
+            orderRepository.createOrder(userRepository.getUserToken(), mProduct.suid!!, mStartDate, mEndDate, message)
+                .doOnSubscribe { loading.value = true }
+                .doOnTerminate { loading.value = false }
+                .subscribe { result ->
+                    if (result.isSuccess() || result.isAcceptable()) {
 
-                } else {
+                        next.value = true
 
-                    errorMessage.value = result.error?.detail
+                    } else {
+
+                        errorMessage.value = result.error?.detail
+                    }
                 }
-            }
-
-
 
 
     }
-
 
 
     override fun onCleared() {
         super.onCleared()
         subscription?.dispose()
     }
-
-
 
 
 }
